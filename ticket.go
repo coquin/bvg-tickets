dbaa9bcda5e3e1061cd42df042758579989fa44d
Denis Kokin <denis.kokin@gmail.com>
2024-05-12 22:03:07 +0200

    Creating time limited ticket

--- ticket.go
+++ ticket.go
@@ -6,13 +6,31 @@ import (
 )
 
 type ticket struct {
-	createdAt time.Time
+	createdAt   time.Time
+	validatedAt time.Time
+	validFor    time.Duration
 }
 
 func New(clock clock.Clock) ticket {
-	return ticket{clock.Now()}
+	return ticket{clock.Now(), time.Time{}, 0}
+}
+
+func NewTimeLimited(clock clock.Clock, duration time.Duration) ticket {
+	return ticket{clock.Now(), time.Time{}, duration}
 }
 
 func (t ticket) CreatedAt() time.Time {
 	return t.createdAt
+}
+
+func (t *ticket) Validate(clock clock.Clock) {
+	t.validatedAt = clock.Now()
+}
+
+func (t ticket) ValidFrom() time.Time {
+	return t.validatedAt
+}
+
+func (t ticket) ValidUntil() time.Time {
+	return t.validatedAt.Add(t.validFor)
 }
--- ticket_test.go
+++ ticket_test.go
@@ -3,9 +3,10 @@ package bvg_tickets_test
 import (
 	bvg_tickets "bvg-tickets"
 	"bvg-tickets/clock"
-	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewTicket(t *testing.T) {
@@ -14,4 +15,18 @@ func TestNewTicket(t *testing.T) {
 	ticket := bvg_tickets.New(mockClock)
 	assert.NotNil(t, ticket)
 	assert.Equal(t, now, ticket.CreatedAt())
+}
+
+func TestNewTimeLimitedTicket(t *testing.T) {
+	now := time.Now()
+	mockClock := clock.Mock(now)
+	twoHours := time.Hour * 2
+	ticket := bvg_tickets.NewTimeLimited(mockClock, twoHours)
+	ticket.Validate(mockClock)
+
+	assert.Equal(t, now, ticket.CreatedAt())
+	assert.Equal(t, now, ticket.ValidFrom())
+
+	twoHoursLater := now.Add(twoHours)
+	assert.Equal(t, twoHoursLater, ticket.ValidUntil())
 }
