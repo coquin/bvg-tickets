788e8d7430e8d2720908d7d6661e04322f8ec5ae
coquin <denis.kokin@gmail.com>
2024-05-12 10:47:28 +0200

    Some work in progress

--- ticket.go
+++ ticket.go
@@ -1,1 +1,18 @@
 package bvg_tickets
+
+import (
+	"bvg-tickets/clock"
+	"time"
+)
+
+type ticket struct {
+	createdAt time.Time
+}
+
+func New(clock clock.Clock) ticket {
+	return ticket{clock.Now()}
+}
+
+func (t ticket) CreatedAt() time.Time {
+	return t.createdAt
+}
--- ticket_test.go
+++ ticket_test.go
@@ -1,1 +1,17 @@
 package bvg_tickets_test
+
+import (
+	bvg_tickets "bvg-tickets"
+	"bvg-tickets/clock"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestNewTicket(t *testing.T) {
+	now := time.Now()
+	mockClock := clock.Mock(now)
+	ticket := bvg_tickets.New(mockClock)
+	assert.NotNil(t, ticket)
+	assert.Equal(t, now, ticket.CreatedAt())
+}
