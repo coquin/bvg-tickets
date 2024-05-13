c63f98185fa7ad484887f839f2ed26d938a49ad7
Denis Kokin <denis.kokin@gmail.com>
2024-05-13 21:41:13 +0200

    Trivial single ticket implemented

--- ticket/ticket.go
+++ ticket/ticket.go
@@ -1,7 +1,37 @@
 package ticket
 
-import "bvg-tickets/clock"
+import (
+	"bvg-tickets/clock"
+	"time"
+)
 
-func Single(clock clock.Clock) string {
-	return ""
+type ticket struct {
+	createdAt     time.Time
+	validatedAt   time.Time
+	startLocation string
+}
+
+func Single(clock clock.Clock) ticket {
+	return ticket{clock.Now(), time.Time{}, ""}
+}
+
+func (t ticket) CreatedAt() time.Time {
+	return t.createdAt
+}
+
+func (t ticket) IsValidated() bool {
+	return !t.validatedAt.Before(t.createdAt)
+}
+
+func (t *ticket) Validate(clock clock.Clock, startLocation string) {
+	t.validatedAt = clock.Now()
+	t.startLocation = startLocation
+}
+
+func (t ticket) ValidFrom() time.Time {
+	return t.validatedAt
+}
+
+func (t ticket) StartLocation() string {
+	return t.startLocation
 }
--- ticket/ticket_test.go
+++ ticket/ticket_test.go
@@ -15,4 +15,14 @@ func TestSingleTicket(t *testing.T) {
 	ticket := ticket.Single(mockClock)
 
 	assert.NotNil(t, ticket)
+	assert.Equal(t, now, ticket.CreatedAt())
+	assert.False(t, ticket.IsValidated())
+
+	later := now.Add(time.Minute)
+	mockClock = clock.Mock(later)
+	ticket.Validate(mockClock, "Berlin Hauptbahnhof")
+
+	assert.True(t, ticket.IsValidated())
+	assert.Equal(t, later, ticket.ValidFrom())
+	assert.Equal(t, "Berlin Hauptbahnhof", ticket.StartLocation())
 }
